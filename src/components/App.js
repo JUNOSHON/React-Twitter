90dba7e7b24a73e2b9460765e0bf87022548322a
junoshon <sanoldfox@naver.com>
2023-05-13 00:06:39 +0900

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 
-import reportWebVitals from "./reportWebVitals";
+import AppRouter from "./AppRouter";
+import {useState} from "react";
+import "firebase/auth"
+import {authServices} from "../fbase";
+
 function App() {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(authServices.currentUser);
   return (
     <div >
-    
+      <AppRouter  isLoggedIn={isLoggedIn}/>
+      <footer>&copy; Made By JUNO {new Date().getFullYear()}</footer>
     </div>
   );
 }
