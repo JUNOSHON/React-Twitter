f2014231212f81edaae8fcbd8045366785a5ced1
junoshon <sanoldfox@naver.com>
2023-05-15 13:24:18 +0900

    solve

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,32 @@
-
 import AppRouter from "./AppRouter";
-import {useState} from "react";
-import "firebase/auth"
+import {useEffect, useState} from "react";
+import "firebase/auth";
 import {authServices} from "../fbase";
+import {logDOM} from "@testing-library/react";
 
-function App() {
+const App = () => {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   
-  const [isLoggedIn, setIsLoggedIn] = useState(authServices.currentUser);
+  
+  
+  useEffect(() => {
+    authServices.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  
   return (
-    <div >
-      <AppRouter  isLoggedIn={isLoggedIn}/>
+    <div>
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Please Init"}
       <footer>&copy; Made By JUNO {new Date().getFullYear()}</footer>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,17 +1,18 @@
 import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+
 import {useState} from "react";
 
 const AppRouter = ({isLoggedIn}) => {
- 
+  
+  
   return (
-    
     <Router>
       <Route exact path="/">
-        {isLoggedIn ? <Home /> : <Auth />}
+        {isLoggedIn ? <Home/> : <Auth/>}
       </Route>
     </Router>
   );
-}
+};
 export default AppRouter;
