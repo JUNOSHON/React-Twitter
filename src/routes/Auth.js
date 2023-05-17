c79345a3f6b29f97333f9642aff0749e7d83b730
junoshon <sanoldfox@naver.com>
2023-05-17 15:10:46 +0900

    add Google Auth

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
-import {authServices} from "../fbase";
+import {authServices, firebaseInstance} from "../fbase";
 
 export default function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const {target: {name, value}} = event;
     console.log(value);
@@ -23,16 +24,27 @@ export default function Auth() {
           data = await authServices.createUserWithEmailAndPassword(email, password,
           );
         } else {
-           data = await authServices.signInWithEmailAndPassword(email, password);
+          data = await authServices.signInWithEmailAndPassword(email, password);
         }
         console.log(data);
       } catch (error) {
-        console.log(error);
+        setError(error.message);
       }
       
     }
   ;
-  
+  const toggleAccount = () => {
+    setNewAccount(prev => !prev);
+  };
+  const onSocialClick=async(event) => {
+    const {target: {name},} = event;
+    let provider;
+    if(name ==='google'){
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }
+    const data = await authServices.signInWithPopup(provider);
+    console.log(data);
+  }
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -40,9 +52,11 @@ export default function Auth() {
         <input name="password" type="password" placeholder={"password"} required value={password}
                onChange={onChange}/>
         <input type="submit" value={newAccount ? "Create Account" : "Log in"}/>
+        {error}
       </form>
+      <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
       <div>
-        <button>Continue with Google</button>
+        <button name="google" onClick={onSocialClick}>Continue with Google</button>
       
       </div>
     
